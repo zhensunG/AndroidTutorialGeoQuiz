97116d820f985a2f977a7df9f419828f5221f0ba
Zhen Sun <zhensun@google.com>
2016-09-20 12:11:10 -0700

    Chapter 2 Finished
    Finished Chapter 2, implemented MVC.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by zhensun on 9/20/16.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
